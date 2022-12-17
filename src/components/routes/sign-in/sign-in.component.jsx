c25eeb2121a63ba80a32b8d75ec622b62f67c815
bartswierz <bartoszswierzynski7@gmail.com>
2022-12-16 22:15:23 -0600

    updating code for 2nd desktop

--- src/components/routes/sign-in/sign-in.component.jsx
+++ src/components/routes/sign-in/sign-in.component.jsx
@@ -2,9 +2,9 @@ import { signInWithGooglePopup } from "../../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
   const logGoogleUser = async () => {
+    // With this response we can see we get back UserCredentialImpl object, inside it we look inside user -> accessToken. This is what we are looking for, this access token was from the flow from our app to firebase and google checking to see that we are who we say we are and giving us back the acessToken.
     const response = await signInWithGooglePopup();
 
-    // With this response we can see we get back UserCredentialImpl object, inside it we look inside user -> accessToken. This is what we are looking for, this access token was from the flow from our app to firebase and google checking to see that we are who we say we are and giving us back the acessToken.
     console.log(response);
   };
 
