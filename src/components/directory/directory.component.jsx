f2a923e0e053dd5993dd3f0c0ba692a4530430d6
bartswierz <bartoszswierzynski7@gmail.com>
2023-03-08 19:34:19 -0600

    installed and integrated tailwind css

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -3,7 +3,7 @@ import CategoryItem from "../category-item/category-item.component";
 
 const Directory = ({ categories }) => {
   return (
-    <div className="directory-container">
+    <div className="directory-container ">
       {categories.map((category) => (
         <CategoryItem category={category} key={category.id} />
       ))}
