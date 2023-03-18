9be8e36e4ac0730ff466ed3d135ed586123c4479
bartswierz <bartoszswierzynski7@gmail.com>
2023-03-17 20:31:31 -0500

    updating shopping site to pull on 2nd PC

--- src/shop-data.js
+++ src/shop-data.js
@@ -188,7 +188,7 @@ const SHOP_DATA = [
         id: 41,
         description: "Basketball Shoes",
         price: 15,
-        imageUrl: "https://i.ibb.co/txNp8Gr/shoes.jpg",
+        imageUrl: "./assets/nike-shoe.png",
       },
       {
         id: 42,
@@ -232,6 +232,54 @@ const SHOP_DATA = [
         price: 15,
         imageUrl: "https://i.ibb.co/txNp8Gr/shoes.jpg",
       },
+      // {
+      //   id: 41,
+      //   description: "Basketball Shoes",
+      //   price: 15,
+      //   imageUrl: "https://i.ibb.co/txNp8Gr/shoes.jpg",
+      // },
+      // {
+      //   id: 42,
+      //   description: "Running Shoes",
+      //   price: 15,
+      //   imageUrl: "https://i.ibb.co/txNp8Gr/shoes.jpg",
+      // },
+      // {
+      //   id: 43,
+      //   description: "Soccer Shoes",
+      //   price: 15,
+      //   imageUrl: "https://i.ibb.co/txNp8Gr/shoes.jpg",
+      // },
+      // {
+      //   id: 44,
+      //   description: "Hiking Boots",
+      //   price: 15,
+      //   imageUrl: "https://i.ibb.co/txNp8Gr/shoes.jpg",
+      // },
+      // {
+      //   id: 45,
+      //   description: "Work Boots",
+      //   price: 15,
+      //   imageUrl: "https://i.ibb.co/txNp8Gr/shoes.jpg",
+      // },
+      // {
+      //   id: 46,
+      //   description: "Dress Shoes",
+      //   price: 15,
+      //   imageUrl: "https://i.ibb.co/txNp8Gr/shoes.jpg",
+      // },
+      // {
+      //   id: 47,
+      //   description: "Converse",
+      //   price: 15,
+      //   imageUrl: "https://i.ibb.co/txNp8Gr/shoes.jpg",
+      // },
+      // {
+      //   id: 48,
+      //   description: "Climbing Shoes",
+      //   price: 15,
+      //   imageUrl: "https://i.ibb.co/txNp8Gr/shoes.jpg",
+      // },
     ],
   },
 ];
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      xs: "320px",
+      sm: "480px",
+      md: "768px",
+      lg: "1020px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        lightGray: "#E8E8E7",
+      },
+      fontFamily: {
+        sans: ["Rubik", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
