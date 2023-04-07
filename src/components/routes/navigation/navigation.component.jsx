ac6613404e64e1a392207a133cb2160a124e3c09
Bart <bartoszswierzynski7@gmail.com>
2023-04-07 02:27:57 -0500

    updated navigation menu

--- src/components/routes/navigation/navigation.component.jsx
+++ src/components/routes/navigation/navigation.component.jsx
@@ -13,6 +13,13 @@ import { FaTimes } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { Icon, useColorScheme } from "@mui/material";
 import { IconContext } from "react-icons/lib";
+import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded";
+import LoginRoundedIcon from "@mui/icons-material/LoginRounded";
+import CreditCardRoundedIcon from "@mui/icons-material/CreditCardRounded";
+
+// Testing logo Icon
+import BrandLogo from "../../../assets/brand-logo.png";
+import NavLogo from "../../../assets/nav-logo.png";
 
 const Navigation = () => {
   const { cartCount } = useContext(CartContext);
@@ -20,10 +27,34 @@ const Navigation = () => {
   // console.log("cartCount: ", cartCount);
   // const [value, setValue] = useState(0);
 
+  const [isMobile, setIsMobile] = useState(false);
+
+  // Check screen width to set navigation items for (mobile < 768px < desktop)
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768); // CHANGE TO MOBILE COMPONENT IF WIDTH LESS THAN 768 px
+    };
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  console.log("isMobile: ", isMobile);
+
   const [click, setClick] = useState(false);
 
+  // document.body.classlist.add("menu-open");
   const handleClick = () => {
     setClick(!click);
+
+    // Adds dark opacity when open
+    document.body.classList.toggle("menu-open");
+
+    // if (click) document.body.classList.add("menu-open");
+    // else document.body.classList.remove("menu-open");
   };
 
   const closeMobileMenu = () => {
@@ -54,8 +85,11 @@ const Navigation = () => {
         <nav className="navbar">
           <div className="navbar-container container">
             {/* <nav className={`navbar ${isMobile ? "mobile" : "desktop"}`}> */}
-            <Link to="/" className="home-logo-container" onClick={closeMobileMenu}>
-              <HomeLogo className="home-logo" />
+            <Link to="/" className="brand-logo-container" onClick={closeMobileMenu}>
+              {/* <HomeLogo className="home-logo" /> */}
+              <div className="filter">
+                <img src={BrandLogo} className="brand-logo" />
+              </div>
               {/* <HomeLogo className="navbar-icon home-logo" onClick={closeMobileMenu} /> */}
             </Link>
             {/* TODO - Turn these into hamburger once screen is 600px */}
@@ -86,18 +120,42 @@ const Navigation = () => {
             <div className="menu-icon" onClick={handleClick}>
               {click ? <FaTimes /> : <GiHamburgerMenu />}
             </div>
+
             <ul className={click ? "nav-menu active" : "nav-menu"}>
-              {/* <li className="nav-item">
+              {/* BRAND LOGO & NAV HEADER TEXT */}
+              {isMobile ? (
+                <li>
+                  <div className="nav-menu-header-container">
+                    <div className="nav-brand-container filter">
+                      <img src={NavLogo} className="brand-logo" />
+                    </div>
+                    <p className="nav-menu-header">Alpha Footwear</p>
+                    {/* <p>Iconic Shoes For Less</p> */}
+                  </div>
+                </li>
+              ) : (
+                ""
+              )}
+              {/* NAV LINKS */}
+              <li className="nav-item">
                 <NavLink to="/" className={({ isActive }) => "nav-links" + (isActive ? " activated" : "")} onClick={closeMobileMenu}>
+                  {isMobile ? (
+                    <div style={{ width: "22px", height: "22px" }}>
+                      <HomeLogo className="home-logo" />
+                    </div>
+                  ) : (
+                    ""
+                  )}
                   Home
                 </NavLink>
-              </li> */}
+              </li>
               <li className="nav-item">
                 <NavLink
                   to="/authentication"
                   className={({ isActive }) => "nav-links" + (isActive ? " activated" : "")}
                   onClick={closeMobileMenu}
                 >
+                  {isMobile ? <LoginRoundedIcon fontSize={"large"} /> : ""}
                   Sign In
                 </NavLink>
               </li>
@@ -107,6 +165,7 @@ const Navigation = () => {
                   className={({ isActive }) => "nav-links" + (isActive ? " activated" : "")}
                   onClick={closeMobileMenu}
                 >
+                  {isMobile ? <CreditCardRoundedIcon fontSize={"large"} /> : ""}
                   checkout
                 </NavLink>
               </li>
@@ -121,6 +180,7 @@ const Navigation = () => {
                     <CartIcon className="cart-icon" />
                     <div className="cart-item-count">{cartCount}</div>
                   </div>
+                  {isMobile ? <span className="menu-link-text">Cart</span> : ""}
                 </NavLink>
               </li>
             </ul>
