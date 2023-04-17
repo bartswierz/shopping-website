e82d9dc6c57de242417dcc8e86439b9a7679693a
Bart <bartoszswierzynski7@gmail.com>
2023-04-17 02:54:24 -0500

    setup project to implement typescript, updated home page, directory and categories to typescript

---
+++ src/components/directory/directory.component.tsx
@@ -0,0 +1,35 @@
+import "./directory.styles.scss";
+import CategoryItem from "../category-item/category-item.component";
+
+//TODO - Create Interface for the props Directory receives
+interface DirectoryProps {
+  categories: {
+    id: number;
+    title: string;
+    imageUrl: string;
+  }[];
+}
+// Option #2 but less info on hover
+// interface DirectoryProps {
+//   categories: categoryOptions[];
+// }
+
+// interface categoryOptions {
+//   id: number;
+//   title: string;
+//   imageUrl: string;
+// }
+
+//HOMEPAGE RENDERS 5 SHOE TYPES(BASKETBALL, SOCCER, OUTDOOR, WORK, CASUAL)
+// const Directory = ({ categories }) => {
+const Directory: React.FC<DirectoryProps> = ({ categories }: DirectoryProps) => {
+  return (
+    <div className="directory-container">
+      {categories.map((category) => (
+        <CategoryItem category={category} key={category.id} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { CartProvider } from "./contexts/cart.context";
 import { CategoriesProvider } from "./contexts/categories.context";
 import { UserProvider } from "./contexts/user.context";
 
+// const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
