01ecb4cb1ac5ed2040f16740983d4a3a0764c75f
Bart <bartoszswierzynski7@gmail.com>
2023-04-29 23:13:45 -0500

    removed cart and categories context providers from main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-// import { CartProvider } from "./contexts/cart.context";
-// import { CategoriesProvider } from "./contexts/categories.context";
 import { UserProvider } from "./contexts/user.context";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
@@ -14,13 +12,9 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        {/* <CategoriesProvider> */}
-        {/* <CartProvider> */}
         <UserProvider>
           <App />
         </UserProvider>
-        {/* </CartProvider> */}
-        {/* </CategoriesProvider> */}
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
